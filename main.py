af3686c6654f8f7f244a886c6e9b47a58edb481b
PatrykNielacny <100844107+PatrykNielacny@users.noreply.github.com>
2022-03-17 10:06:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+lista_gene1 = ['SLC19A2', 'ATP7B', 'ERBB3', 'FGFR14', 'ABCC3','GALNT14', 'ERCC1',
+              'LJS19A2', 'AKM7B', 'ELLB34', 'FULR4', 'ANGC3', 'WELNT14', 'EOO1',                    'SAC19A22', 'FGFR4', 'ERB3', 'FGR4', 'FGFR4', 'GASNT14', 'ERSS4']
+
+if("FGERA4" in lista_gene1):
+  print("Gen FGERA4 znajduje sie na indexie nr:",lista_gene1.index("FGERA4"))
+else:
+  print("Gen FGERA4 nie zawiera sie w liscie.")
+if("FGFR4" in lista_gene1):
+  print("Gen FGFR4 znajduje sie na indexie nr:",lista_gene1.index("FGFR4"))
+else:
+  print("Gen FGFR4 nie zawiera sie w liscie.")
